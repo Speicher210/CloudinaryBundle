0d4286675f2ed4d7bc30336adf04cac68870fab1
Dragos Protung <dragos@protung.ro>
2016-04-18 16:49:08 +0200

    Added command to show info about a resource.

---
+++ Command/InfoCommand.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Speicher210\CloudinaryBundle\Command;
+
+use Cloudinary\Api\Response;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command to get info about a resource.
+ */
+class InfoCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('sp210:cloudinary:info')
+            ->setDescription('Get information about a resource.')
+            ->addArgument('public_id', InputArgument::REQUIRED, 'The public ID of the resource.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $api = $this->getContainer()->get('speicher210_cloudinary.api');
+
+        $response = $api->resource($input->getArgument('public_id'));
+
+        $this->renderProperties($output, $response);
+
+        if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+            $output->writeln(PHP_EOL);
+            $this->renderDerivedResources($output, $response['derived']);
+        }
+    }
+
+    /**
+     * Render the general properties.
+     *
+     * @param OutputInterface $output The output.
+     * @param Response $response The API response.
+     */
+    protected function renderProperties(OutputInterface $output, Response $response)
+    {
+        $table = new Table($output);
+        $table->setHeaders(array('Property', 'Value'));
+        foreach ($response as $property => $value) {
+            if (is_scalar($value)) {
+                $table->addRow(array($property, $value));
+            }
+        }
+        $table->render();
+    }
+
+    /**
+     * Render the derived resources.
+     *
+     * @param OutputInterface $output The output.
+     * @param array $derivedResources The derived resources.
+     */
+    protected function renderDerivedResources(OutputInterface $output, array $derivedResources)
+    {
+        $table = new Table($output);
+        $table->setHeaders(
+            array(
+                array(new TableCell('Derived resources', array('colspan' => 5))),
+                array('ID', 'Format', 'Size', 'Transformation', 'URL')
+            )
+        );
+        foreach ($derivedResources as $resource) {
+            $table->addRow(
+                array(
+                    $resource['id'],
+                    $resource['format'],
+                    $this->formatSize($resource['bytes']),
+                    $resource['transformation'],
+                    $resource['url']
+                )
+            );
+        }
+        $table->render();
+    }
+
+    /**
+     * Format the size of a file.
+     *
+     * @param integer $bytes The number of bytes.
+     *
+     * @return string
+     */
+    private function formatSize($bytes)
+    {
+        $unit = 1024;
+        if ($bytes <= $unit) {
+            return $bytes . " b";
+        }
+        $exp = intval((log($bytes) / log($unit)));
+        $pre = "kMGTPE";
+        $pre = $pre[$exp - 1];
+
+        return sprintf("%.1f %sB", $bytes / pow($unit, $exp), $pre);
+    }
+}
