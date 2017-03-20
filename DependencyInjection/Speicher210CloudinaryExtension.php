e15758f054e261ab32951110f6675df066a0e5cb
Dragos Protung <dragos@protung.ro>
2017-03-20 13:26:31 +0100

    Fixed extension

--- DependencyInjection/Speicher210CloudinaryExtension.php
+++ DependencyInjection/Speicher210CloudinaryExtension.php
@@ -20,34 +20,8 @@ class Speicher210CloudinaryExtension extends ConfigurableExtension
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
 
-        if (array_key_exists('url', $config)) {
-            $url = parse_url($config['url']);
-
-            if (array_key_exists('host', $url)) {
-                $config['cloud_name'] = $url['host'];
-            }
-
-            if (array_key_exists('user', $url)) {
-                $config['access_identifier']['api_key'] = $url['user'];
-            }
-            if (array_key_exists('pass', $url)) {
-                $config['access_identifier']['api_secret'] = $url['pass'];
-            }
-        }
-
-        if (!isset($config['cloud_name'], $config['access_identifier']['api_key'], $config['access_identifier']['api_secret'])) {
-            throw new \InvalidArgumentException('Cloudinary URL not correctly set.');
-        }
-
         $container
             ->getDefinition('speicher210_cloudinary.cloudinary')
-            ->replaceArgument(
-                0,
-                [
-                    'cloud_name' => $config['cloud_name'],
-                    'api_key' => $config['access_identifier']['api_key'],
-                    'api_secret' => $config['access_identifier']['api_secret'],
-                ]
-            );
+            ->replaceArgument(0, $config);
     }
 }
